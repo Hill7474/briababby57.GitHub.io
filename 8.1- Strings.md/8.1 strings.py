350d6846cc0cb786b666927c99e68a55c47a0d77
Hill7474 <135084696+Hill7474@users.noreply.github.com>
2023-09-17 07:11:22 -0500

    Create 8.1 strings.py

---
+++ 8.1- Strings.md/8.1 strings.py
@@ -0,0 +1,49 @@
+ 8.1 
+# Strings
+strings surrounded by single or doible quotations 
+'hello' same as "hello"
+
+
+str1 = 'Hello, world!'
+str2 = "Python is fun."
+str3 = '''This is a 
+           multiline
+           string.'''
+print("Strings can be manipulated in various ways here are some string operations")
+
+# Simple String Operations
+
+# 1. Concatenation
+x = "I love, "
+y = "you"
+print(x + y)
+
+# 2. Repetition
+x = "xyz"
+print(x * 3)
+
+# 3. String Indexing
+b = "Python"
+print(c[0])
+
+# 4. String Slicing
+c = "Python is fun"
+print(c[0:6])
+
+# 5. String Length
+b = "Py"
+print(len(b))
+
+# 6. String Methods
+upperStr = str1.upper()
+lowerStr = str2.lower()
+replaceStr = str3.replace("This","That")
+print(upperStr)
+print(lowerStr)
+print(replaceStr)
+
+
+
+
+
+
