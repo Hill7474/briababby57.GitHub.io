c1e82f0b597ed906b7dedf262908da71e08fdcc9
Hill7474 <135084696+Hill7474@users.noreply.github.com>
2023-09-29 07:17:02 -0500

    Update Errors.py

--- 1.2 variables & types/Errors.py
+++ 1.2 variables & types/Errors.py
@@ -1,10 +1,18 @@
 print("Error snippets")
 # some common errors people often make
 
-
+Syntax error
 # Bad
 my variable = input("WHO GOES THERE? ")
 print(my variable)
 # Good 
-myVariable = input("WHO GOES THERE?")
-print(myVariable)
+my_variable = input("WHO GOES THERE?")
+print(my_variable)
+
+Name error
+# Bad 
+myGrandma = input("How's your Grandma doing? 😘 ")
+print(mygrandma) # mygrandma wasn't defined
+# Good
+myGrandma = input("How-s your Grandma doing?")
+print(myGrandma)
