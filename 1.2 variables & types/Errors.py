352f20dd7be35041254fb2c8a5c971c22c6b2be2
Hill7474 <135084696+Hill7474@users.noreply.github.com>
2023-09-29 06:38:28 -0500

    Create Errors.py

---
+++ 1.2 variables & types/Errors.py
@@ -0,0 +1,10 @@
+print("Error snippets")
+# some common errors people often make
+
+
+# Bad
+my variable = input("WHO GOES THERE? ")
+print(my variable)
+# Good 
+myVariable = input("WHO GOES THERE?")
+print(myVariable)
